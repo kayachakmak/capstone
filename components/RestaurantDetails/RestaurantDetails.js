a87b4bda25e7e312a2eb6d14b60a3cb99fb117aa
Kaya Chakmak <kycakmak@gmail.com>
2023-11-29 17:08:58 +0100

    add details page

---
+++ components/RestaurantDetails/RestaurantDetails.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const customLoader = ({ src }) => {
+  return src;
+};
+export default function RestaurantDetails({ restaurant }) {
+  return (
+    <>
+      <h1>{restaurant.name}</h1>
+      <Image
+        loader={customLoader}
+        src={restaurant.image}
+        alt={restaurant.name}
+        width={300}
+        height={400}
+      />
+      <Link href={restaurant.link} target="_blank">
+        Visit the {restaurant.name}`s website
+      </Link>
+      <Link href={restaurant.menu} target="_blank">
+        See the menu
+      </Link>
+    </>
+  );
+}
